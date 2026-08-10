4e6d4b41037a8418c0695258e6afbd7003692efe
qb1ty <arsenbro0707@mail.ru>
2026-08-10 23:50:58 +0500

    chore: connection to the auth microservice is configured

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from "@nestjs/core"
+import { Transport, type MicroserviceOptions } from "@nestjs/microservices"
+
+import { AppModule } from "./app.module"
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+
+	app.connectMicroservice<MicroserviceOptions>({
+		transport: Transport.GRPC,
+		options: {
+			package: "auth.v1",
+			protoPath: "node_modules/@qb1tycinema/contracts/proto/auth.proto",
+			url: "localhost:50051",
+			loader: {
+				keepCase: false,
+				longs: String,
+				enums: String,
+				defaults: true,
+				oneofs: true
+			}
+		}
+	})
+
+	await app.startAllMicroservices()
+	await app.init()
+}
+bootstrap()
