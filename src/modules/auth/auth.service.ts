2f96c3e71f42f1f61294068ac8fe1ce71840f378
qb1ty <arsenbro0707@mail.ru>
2026-08-13 03:14:44 +0500

    feat: sendOtp implemented

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, NotFoundException } from "@nestjs/common"
+import type { Account } from "@orm/generated/client"
+import type {
+	SendOtpRequest,
+	SendOtpResponse
+} from "@qb1tycinema/contracts/gen/auth"
+
+import { AuthRepository } from "./auth.repository"
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	public constructor(private readonly authRepository: AuthRepository) {}
+
+	public async sendOtp(data: SendOtpRequest): Promise<SendOtpResponse> {
+		const { identifier, type } = data
+
+		let account!: Account | null
+
+		if (type === "phone") {
+			account = await this.authRepository.findByPhone(identifier)
+		} else if (type === "email") {
+			account = await this.authRepository.findByEmail(identifier)
+		}
+
+		if (!account) {
+			account = await this.authRepository.createAccount({
+				email: type === "email" ? identifier : undefined,
+				phone: type === "phone" ? identifier : undefined
+			})
+		}
+
+		return {
+			ok: true
+		}
+	}
+}
