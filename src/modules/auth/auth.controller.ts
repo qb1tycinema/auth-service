b910d269b5d241aca8d1993d03a5464f4ca05449
qb1ty <arsenbro0707@mail.ru>
2026-08-13 03:12:41 +0500

    feat: call to the service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -13,8 +13,6 @@ export class AuthController {
 
 	@GrpcMethod("AuthService", "SendOtp")
 	public async sendOtp(data: SendOtpRequest): Promise<SendOtpResponse> {
-		console.log("Incoming OTP request: ", data)
-
-		return { ok: true }
+		return await this.authService.sendOtp(data)
 	}
 }
