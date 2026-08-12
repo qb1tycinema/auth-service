394793f35c77b36471d96d155a5f9e9a15022012
qb1ty <arsenbro0707@mail.ru>
2026-08-13 03:50:08 +0500

    chore: connect redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 
 import { PrismaModule } from "./infrastructure/prisma/prisma.module"
+import { RedisModule } from "./infrastructure/redis/redis.module"
 import { AuthModule } from "./modules/auth/auth.module"
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		PrismaModule,
+		RedisModule,
 		AuthModule
 	]
 })
