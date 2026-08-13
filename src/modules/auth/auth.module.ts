6e4664c9bd706b0be2b6224c6dbbfc69dfb41c54
qb1ty <arsenbro0707@mail.ru>
2026-08-14 02:36:10 +0500

    chore: connect otp service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common"
 
+import { OtpService } from "../otp/otp.service"
+
 import { AuthController } from "./auth.controller"
 import { AuthRepository } from "./auth.repository"
 import { AuthService } from "./auth.service"
 
 @Module({
 	controllers: [AuthController],
-	providers: [AuthService, AuthRepository]
+	providers: [AuthService, AuthRepository, OtpService]
 })
 export class AuthModule {}
