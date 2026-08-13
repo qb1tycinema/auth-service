c2591a4e2dbd67b5afe1d44964952adb69a78a1d
qb1ty <arsenbro0707@mail.ru>
2026-08-14 02:35:27 +0500

    feat: new method added

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,23 @@
 import { Injectable, NotFoundException } from "@nestjs/common"
+import { RpcException } from "@nestjs/microservices"
 import type { Account } from "@orm/generated/client"
 import type {
 	SendOtpRequest,
-	SendOtpResponse
+	SendOtpResponse,
+	VerifyOtpRequest,
+	VerifyOtpResponse
 } from "@qb1tycinema/contracts/gen/auth"
+
+import { OtpService } from "../otp/otp.service"
 
 import { AuthRepository } from "./auth.repository"
 
 @Injectable()
 export class AuthService {
-	public constructor(private readonly authRepository: AuthRepository) {}
+	public constructor(
+		private readonly authRepository: AuthRepository,
+		private readonly otpService: OtpService
+	) {}
 
 	public async sendOtp(data: SendOtpRequest): Promise<SendOtpResponse> {
 		const { identifier, type } = data
@@ -29,8 +37,54 @@ export class AuthService {
 			})
 		}
 
+		const code = await this.otpService.send(
+			identifier,
+			type as "phone" | "email"
+		)
+
+		console.log(code)
+
 		return {
 			ok: true
+		}
+	}
+
+	public async verifyOtp(data: VerifyOtpRequest): Promise<VerifyOtpResponse> {
+		const { identifier, code, type } = data
+
+		await this.otpService.verify(
+			identifier,
+			code,
+			type as "email" | "phone"
+		)
+
+		let account!: Account | null
+
+		if (type === "phone") {
+			account = await this.authRepository.findByPhone(identifier)
+		} else if (type === "email") {
+			account = await this.authRepository.findByEmail(identifier)
+		}
+
+		if (!account) {
+			throw new RpcException("Account not found")
+		}
+
+		if (type === "phone" && !account.isPhoneVerified) {
+			await this.authRepository.update(account.id, {
+				isPhoneVerified: true
+			})
+		}
+
+		if (type === "email" && !account.isEmailVerified) {
+			await this.authRepository.update(account.id, {
+				isEmailVerified: true
+			})
+		}
+
+		return {
+			accessToken: "123456",
+			refreshToken: "123456"
 		}
 	}
 }
