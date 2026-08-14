a948a63bf6bc6ced5b6ac81b23a45720eb07a344
qb1ty <arsenbro0707@mail.ru>
2026-08-15 00:58:33 +0500

    refactor: refactoring main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,16 @@
+import { ConfigService } from "@nestjs/config"
 import { NestFactory } from "@nestjs/core"
-import { type MicroserviceOptions, Transport } from "@nestjs/microservices"
 
 import { AppModule } from "./app.module"
+import type { AllConfigs } from "./config"
+import { createGrpcServer } from "./infrastructure/grpc"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
-	app.connectMicroservice<MicroserviceOptions>({
-		transport: Transport.GRPC,
-		options: {
-			package: "auth.v1",
-			protoPath: "node_modules/@qb1tycinema/contracts/proto/auth.proto",
-			url: "localhost:50051",
-			loader: {
-				keepCase: false,
-				longs: String,
-				enums: String,
-				defaults: true,
-				oneofs: true
-			}
-		}
-	})
+	const config = app.get(ConfigService<AllConfigs>)
+
+	createGrpcServer(app, config)
 
 	await app.startAllMicroservices()
 	await app.init()
