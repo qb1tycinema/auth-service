992244cfbc4f8339cd1eeeb00ee784650e472f84
qb1ty <arsenbro0707@mail.ru>
2026-08-15 00:45:16 +0500

    refactor: logic for creating and connecting the microserive has been moved to a separate directory

---
+++ src/infrastructure/grpc/grpc.server.ts
@@ -0,0 +1,20 @@
+import type { AllConfigs } from "@/config";
+import type { INestApplication } from "@nestjs/common";
+import  { ConfigService } from "@nestjs/config";
+import { type MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { grpcLoader, grpcPackages, grpcProtoPaths } from "./grpc.options";
+
+export function createGrpcServer(app: INestApplication, config: ConfigService<AllConfigs>) {
+    const host = config.get("grpc.host", { infer: true })
+	const port = config.get("grpc.port", { infer: true })
+
+	app.connectMicroservice<MicroserviceOptions>({
+		transport: Transport.GRPC,
+		options: {
+			package: grpcPackages,
+			protoPath: grpcProtoPaths,
+			url: `${host}:${port}`,
+			loader: grpcLoader
+		}
+	})
+}
