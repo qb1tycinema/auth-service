eb4ca51bda4c561505620704423f27c8f3a7f051
qb1ty <arsenbro0707@mail.ru>
2026-08-19 23:53:45 +0500

    feat: configure async PassportModule integration

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { Module } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
+import { PassportModule } from "@qb1tycinema/passport"
 
 import { OtpService } from "../otp/otp.service"
 
 import { AuthController } from "./auth.controller"
 import { AuthRepository } from "./auth.repository"
 import { AuthService } from "./auth.service"
+import { getPassportConfig } from "@/config/loaders"
 
 @Module({
+	imports: [
+		PassportModule.registerAsync({
+			useFactory: getPassportConfig,
+			inject: [ConfigService]
+		})
+	],
 	controllers: [AuthController],
 	providers: [AuthService, AuthRepository, OtpService]
 })
