e492c7d7dedb383c42f7e24e915b73c61a63e540
qb1ty <arsenbro0707@mail.ru>
2026-08-19 23:56:07 +0500

    feat: add passport config interface

--- src/config/interfaces/all-configs.interface.ts
+++ src/config/interfaces/all-configs.interface.ts
@@ -1,9 +1,11 @@
 import type { DatabaseConfig } from "./database.interface"
 import type { GrpcConfig } from "./grpc.interface"
+import type { PassportConfig } from "./passport.interface"
 import type { RedisConfig } from "./redis.interface"
 
 export interface AllConfigs {
 	database: DatabaseConfig
 	grpc: GrpcConfig
+	passport: PassportConfig
 	redis: RedisConfig
 }
