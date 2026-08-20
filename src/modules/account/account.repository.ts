7abb290aedfbb2f388929aa1e1a5dae78588b1bb
qb1ty <arsenbro0707@mail.ru>
2026-08-21 01:01:42 +0500

    feat: add account repository for database access

---
+++ src/modules/account/account.repository.ts
@@ -0,0 +1,14 @@
+import { PrismaService } from "@/infrastructure/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
+import type { Account } from "@orm/generated/client";
+
+@Injectable()
+export class AccountRepository {
+    public constructor(
+        private readonly prisma: PrismaService
+    ) {}
+
+    public findById(id: string): Promise<Account | null> {
+        return this.prisma.account.findUnique({ where: { id } })
+    }
+}
