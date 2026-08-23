193b56118ee25e731d9aff5156b13464c2ff0d47
qb1ty <arsenbro0707@mail.ru>
2026-08-23 19:09:48 +0500

    refactor(account): wire up UserRepository and OtpService dependencies

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common"
 
+import { OtpService } from "../otp/otp.service"
+
 import { AccountController } from "./account.controller"
 import { AccountRepository } from "./account.repository"
 import { AccountService } from "./account.service"
+import { UserRepository } from "@/shared/repositories"
 
 @Module({
 	controllers: [AccountController],
-	providers: [AccountService, AccountRepository]
+	providers: [AccountService, AccountRepository, UserRepository, OtpService]
 })
 export class AccountModule {}
