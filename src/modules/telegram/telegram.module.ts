631f57520db5ef3dddacc255fa155f15fd924868
qb1ty <arsenbro0707@mail.ru>
2026-08-27 04:43:32 +0500

    chore(telegram): register UserRepository in module providers

--- src/modules/telegram/telegram.module.ts
+++ src/modules/telegram/telegram.module.ts
@@ -5,9 +5,10 @@ import { TokenService } from "../token/token.service"
 import { TelegramController } from "./telegram.controller"
 import { TelegramRepository } from "./telegram.repository"
 import { TelegramService } from "./telegram.service"
+import { UserRepository } from "@/shared/repositories"
 
 @Module({
 	controllers: [TelegramController],
-	providers: [TelegramService, TelegramRepository, TokenService]
+	providers: [TelegramService, TelegramRepository, TokenService, UserRepository]
 })
 export class TelegramModule {}
