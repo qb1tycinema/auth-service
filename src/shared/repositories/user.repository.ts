15b244a00932566b760201c33f4accdaa825d3bb
qb1ty <arsenbro0707@mail.ru>
2026-08-27 04:40:45 +0500

    feat(user): add phone and email lookups along with CRUD methods to repository

--- src/shared/repositories/user.repository.ts
+++ src/shared/repositories/user.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common"
 import type { Account } from "@orm/generated/client"
-import type { AccountUpdateInput } from "@orm/generated/models"
+import type { AccountCreateInput, AccountUpdateInput } from "@orm/generated/models"
 
 import { PrismaService } from "@/infrastructure/prisma/prisma.service"
 
@@ -14,6 +14,12 @@ export class UserRepository {
 
 	public async findByEmail(email: string): Promise<Account | null> {
 		return await this.prisma.account.findUnique({ where: { email } })
+	}
+
+	public async createAccount(
+		data: AccountCreateInput
+	): Promise<Account | null> {
+		return await this.prisma.account.create({ data })
 	}
 
 	public async update(
