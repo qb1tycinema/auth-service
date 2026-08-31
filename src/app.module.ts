02dc3ad7e47f820b285a5e079c12464e557cdb4f
qb1ty <arsenbro0707@mail.ru>
2026-09-01 00:14:31 +0500

    chore: register messaging module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import {
 	redisEnv,
 	telegramEnv
 } from "./config"
+import { MessagingModule } from "./infrastructure/messaging/messaging.module"
 import { PrismaModule } from "./infrastructure/prisma/prisma.module"
 import { RedisModule } from "./infrastructure/redis/redis.module"
 import { AccountModule } from "./modules/account/account.module"
@@ -24,6 +25,7 @@ import { TokenModule } from "./modules/token/token.module"
 		}),
 		PrismaModule,
 		RedisModule,
+		MessagingModule,
 		AuthModule,
 		OtpModule,
 		AccountModule,
